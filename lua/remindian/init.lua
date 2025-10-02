91a3c2dda29ddcfef54927ee475751f7781abe46
Sebastien Le Digabel <sledigabel@gmail.com>
2025-10-02 14:04:32 +0100

    Adapting the plugin

--- lua/remindian/init.lua
+++ lua/remindian/init.lua
@@ -1,17 +1,18 @@
 local M = {}
 
 -- Import the configuration module
-local config = require('remindian.config')
+local config = require("remindian.config")
 
 -- Setup function for plugin configuration
 function M.setup(opts)
-    -- Override defaults with user provided options
-    config.setup(opts)
-    
-    -- Register commands
-    vim.api.nvim_create_user_command('RemindianHello', function()
-        vim.notify('Hello from Remindian plugin!', vim.log.levels.INFO)
-    end, {})
+  -- Override defaults with user provided options
+  config.setup(opts)
+
+  -- adding an autocmd to trigger reminders on Save
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.md", -- Adjust the pattern as needed
+    command = "lua require('remindian.reminder').trigger()",
+  })
 end
 
 return M
---
+++ lua/remindian/reminder.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.trigger = function()
+  -- placeholder for the actual logic
+  vim.notify("Reminder triggered!")
+end
+
+return M
